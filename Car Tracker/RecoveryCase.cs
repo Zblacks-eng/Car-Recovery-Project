3b2063ad41091c67cdb12b5684a949a7a29dd43b
brentlee574 <brentlee574@gmail.com>
2026-03-17 15:41:44 +0000

    Add files via upload

---
+++ Car Tracker/RecoveryCase.cs
@@ -0,0 +1,19 @@
+﻿namespace MobileCarRecoverySystem.Models
+{
+    public class RecoveryCase
+    {
+        public int CaseId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string VehicleRegistration { get; set; }
+
+        public string VehicleModel { get; set; }
+
+        public string BreakdownLocation { get; set; }
+
+        public string RecoveryStatus { get; set; }
+    }
+}
