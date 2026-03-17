5693ac4cadeccd032064ddf3dc43e71137b61b6c
brentlee574 <brentlee574@gmail.com>
2026-03-17 15:50:25 +0000

    Add files via upload

---
+++ Car Tracker/Models/UserAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace MobileCarRecoverySystem.Models
+{
+    public class UserAccount
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
