090a4d42f8feef5c5879921484933ae2352ea998
brentlee574 <brentlee574@gmail.com>
2026-03-17 15:34:56 +0000

    Delete Car Tracker/UserAccount.cs

--- Car Tracker/UserAccount.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MobileCarRecoverySystem.Models
-{
-    public class UserAccount
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
